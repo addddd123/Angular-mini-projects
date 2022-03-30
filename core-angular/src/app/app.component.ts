002e9984b3932d4839e17236d96e493c0beaa372
addddd123 <adshe886@gmail.com>
2022-03-30 14:57:45 +0530

    core angular ,component

---
+++ core-angular/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"my-app",
+    // template:`<h1>Hey angular</h1>`,
+    templateUrl:"./app.component.html",
+    // styles:[`
+    // h1{
+    //     background-color:#fff;
+    //     text-align:center;
+    // }
+    // `]
+    styleUrls:["./app.component.css"],
+})
+
+export class AppComponent{}
